9c2afddebb1061d364df74fe2d47699d1e6f7c8f
Hurlink Vongsachang <hvongsachang@college.harvard.edu>
2018-10-16 14:46:11 -0400

    added tests

--- test_roots.py
+++ test_roots.py
@@ -1,6 +1,13 @@
 import pytest
 import roots
 
+def test_linearroots_result():
+    assert roots.linear_roots(1.0, 2.0) == -2.0
+
+def test_linearroots_types():
+    with pytest.raises(TypeError):
+        roots.linear_roots("hello", "world")
+
 def test_quadroots_result():
     assert roots.quad_roots(1.0, 1.0, -12.0) == ((3+0j), (-4+0j))
 
